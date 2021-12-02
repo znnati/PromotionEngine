e76f8a99c9bd52235fd0c28a66cb10b1e3835977
Benaissa Znnati <znnati@outlook.com>
2021-12-02 17:40:27 +0100

    Added QuantityPromotionTest and CombinationPromotionTest set to fail.

--- PromotionEngine.Test/PromotionServiceTests.cs
+++ PromotionEngine.Test/PromotionServiceTests.cs
@@ -23,6 +23,28 @@ namespace PromotionEngine.Test
             DbProductsList = new List<(char Sku, double Price)> { ('A', 50), ('B', 30), ('C', 20), ('D', 15) };
         }
 
+
+
+        [Test]
+        [TestCase('A', 1, 50)]
+        [TestCase('A', 2, 100)]
+        [TestCase('A', 3, 130)]
+        [TestCase('A', 5, 230)]
+        public void QuantityPromotionTest(char sku, int quantity, decimal total)
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        [TestCaseSource(nameof(GetCombinationPromotion_Case_1))]
+        [TestCaseSource(nameof(GetCombinationPromotion_Case_2))]
+        [TestCaseSource(nameof(GetCombinationPromotion_Case_3))]
+        [TestCaseSource(nameof(GetCombinationPromotion_Case_4))]
+        public void CombinationPromotionTest(IList<(char Sku, int price)> data, double total)
+        {
+            Assert.Fail();
+        }
+
         [Test]
         [TestCaseSource(nameof(GetScenario_A_Data))]
         [TestCaseSource(nameof(GetScenario_B_Data))]
@@ -44,6 +66,24 @@ namespace PromotionEngine.Test
         private static IEnumerable<object[]> GetScenario_C_Data()
         {
             yield return new object[] { new List<(char Sku, int price)>() { ('A', 3), ('B', 5), ('C', 1), ('D', 1) }, 280.0 };
+        }
+
+
+        private static IEnumerable<object[]> GetCombinationPromotion_Case_1()
+        {
+            yield return new object[] { new List<(char Sku, int price)>() { ('C', 1), ('D', 1) }, 30.0 };
+        }
+        private static IEnumerable<object[]> GetCombinationPromotion_Case_2()
+        {
+            yield return new object[] { new List<(char Sku, int price)>() { ('C', 3), ('D', 2) }, 80.0 };
+        }
+        private static IEnumerable<object[]> GetCombinationPromotion_Case_3()
+        {
+            yield return new object[] { new List<(char Sku, int price)>() { ('A', 1), ('B', 1), ('C', 1), ('D', 1) }, 110.0 };
+        }
+        private static IEnumerable<object[]> GetCombinationPromotion_Case_4()
+        {
+            yield return new object[] { new List<(char Sku, int price)>() { ('A', 2), ('B', 2), ('C', 2), ('D', 4) }, 250.0 };
         }
     }
 }

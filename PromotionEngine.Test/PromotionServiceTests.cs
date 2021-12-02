5ed16557fc9383a280c52e5da0cd9150402563ea
Benaissa Znnati <znnati@outlook.com>
2021-12-02 17:21:04 +0100

    Added a ScenarioTest set to fail and data for the 3 scenarios.

--- PromotionEngine.Test/PromotionServiceTests.cs
+++ PromotionEngine.Test/PromotionServiceTests.cs
@@ -19,13 +19,30 @@ namespace PromotionEngine.Test
                 new CombinationPromotion("C & D for 30", skuQuntities: new [] {('C', 1), ('D', 1)}, price: 30)
             };
 
-        DbProductsList = new List<(char Sku, double Price)> { ('A', 50), ('B', 30), ('C', 20), ('D', 15) };
-    }
+            DbProductsList = new List<(char Sku, double Price)> { ('A', 50), ('B', 30), ('C', 20), ('D', 15) };
+        }
 
-    [Test]
-        public void Test1()
+        [Test]
+        [TestCaseSource(nameof(GetScenario_A_Data))]
+        [TestCaseSource(nameof(GetScenario_B_Data))]
+        [TestCaseSource(nameof(GetScenario_C_Data))]
+        public void ScenarioTest(IList<(char Sku, int price)> data, double total)
         {
-            Assert.Pass();
+            Assert.Fail();
+        }
+
+        private static IEnumerable<object[]> GetScenario_A_Data()
+        {
+            yield return new object[] { new List<(char Sku, int price)>() { ('A', 1), ('B', 1), ('C', 1) }, 100.0 };
+        }
+
+        private static IEnumerable<object[]> GetScenario_B_Data()
+        {
+            yield return new object[] { new List<(char Sku, int price)>() { ('A', 5), ('B', 5), ('C', 1) }, 370.0 };
+        }
+        private static IEnumerable<object[]> GetScenario_C_Data()
+        {
+            yield return new object[] { new List<(char Sku, int price)>() { ('A', 3), ('B', 5), ('C', 1), ('D', 1) }, 280.0 };
         }
     }
 }

c6c489f9b2578c3845b3477212822f9874769999
Benaissa Znnati <znnati@outlook.com>
2021-12-02 21:11:29 +0100

    Added implementation for TryApplyQuantityPromotionOnItem.

--- PromotionEngine/Services/PromotionService.cs
+++ PromotionEngine/Services/PromotionService.cs
@@ -11,12 +11,29 @@ namespace PromotionEngine.Services
 
         public BasketPromotionItem? TryApplyQuantityPromotionOnItem(BasketItem item, QuantityPromotion quantityPromotion)
         {
-            throw new NotImplementedException();
+            // Check if item has the given quantity promotion. Return null if not.
+            if (!HasQuantityPromotion(item, quantityPromotion))
+                return null;
+
+            // Reduce the item quantity by the nbr used in the promotion.
+            item.Quantity -= quantityPromotion.Nbr;
+
+            // Keep trying applyin the promotion and get a new object while it is applicable
+            var promotedItem = TryApplyQuantityPromotionOnItem(item, quantityPromotion);
+
+            // Return the result of the method's concurrency
+            return new BasketPromotionItem { Sku = item.Sku, Quantity = (promotedItem?.Quantity ?? 0) + quantityPromotion.Nbr, Total = (promotedItem?.Total ?? 0) + quantityPromotion.Price };
         }
 
         public IList<BasketPromotionItem> TyrApplyPromotionsOnItems(IEnumerable<BasketItem> items, IList<Promotion> activePromotions)
         {
             throw new NotImplementedException();
+        }
+
+
+        private bool HasQuantityPromotion(BasketItem item, QuantityPromotion promotion)
+        {
+            return false;
         }
     }
 }

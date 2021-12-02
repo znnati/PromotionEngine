cb82c1bb14cb38f19a31cce57a77f9a8be62eaea
Benaissa Znnati <znnati@outlook.com>
2021-12-02 17:19:15 +0100

    Added setup data.

--- PromotionEngine.Test/PromotionServiceTests.cs
+++ PromotionEngine.Test/PromotionServiceTests.cs
@@ -1,15 +1,28 @@
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace PromotionEngine.Test
 {
     public class Tests
     {
+        IList<Promotion> ActivePromotions { get; set; } = new List<Promotion>();
+
+        IList<(char Sku, double Price)> DbProductsList { get; set; } = new List<(char Sku, double Price)>();
+
         [SetUp]
         public void Setup()
         {
-        }
+            ActivePromotions = new List<Promotion>()
+            {
+                new QuantityPromotion(name: "3 of A's for 130", sku: 'A', nbr: 3, price: 130),
+                new QuantityPromotion("2 of B's for 45", 'B', 2, 45),
+                new CombinationPromotion("C & D for 30", skuQuntities: new [] {('C', 1), ('D', 1)}, price: 30)
+            };
 
-        [Test]
+        DbProductsList = new List<(char Sku, double Price)> { ('A', 50), ('B', 30), ('C', 20), ('D', 15) };
+    }
+
+    [Test]
         public void Test1()
         {
             Assert.Pass();
